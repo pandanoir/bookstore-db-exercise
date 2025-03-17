c8c5d13e5b9023f0cf1155212ad9436c30fc7d1c
pandanoir <pandanoir@ymail.ne.jp>
2025-03-17 23:23:36 +0900

    add sql directory

--- seed.sh
+++ seed.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 CONTAINER_NAME="my-mysql"
 
-docker exec -i $CONTAINER_NAME mysql --defaults-extra-file=~/.my.cnf < table.sql
-docker exec -i $CONTAINER_NAME mysql --defaults-extra-file=~/.my.cnf < seed.sql
+docker exec -i $CONTAINER_NAME mysql --defaults-extra-file=~/.my.cnf < sql/schema/table.sql
+docker exec -i $CONTAINER_NAME mysql --defaults-extra-file=~/.my.cnf < sql/query/seed.sql
 echo 'USE bookstore; SHOW TABLES' | docker exec -i $CONTAINER_NAME mysql --defaults-extra-file=~/.my.cnf
 echo 'USE bookstore; SELECT * FROM Authors' | docker exec -i $CONTAINER_NAME mysql --defaults-extra-file=~/.my.cnf
 
